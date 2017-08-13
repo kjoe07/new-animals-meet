9f6b000d0bc5e361cbfbf5147e8e87f811ad6de0
Reynaldo Aguilar <rlac1990@gmail.com>
2017-08-13 18:35:37 -0400

    DONE RF26, RF36

--- AnimalsMeet/AnimalListTableViewController.swift
+++ AnimalsMeet/AnimalListTableViewController.swift
@@ -13,65 +13,69 @@ import Sensitive
 import PromiseKit
 
 class AnimalListTableViewController: EasyTableViewController<AnimalModel, AnimalListTableViewCell> {
-    
-    var user: UserModel!
-    var createAnimal: (() -> ())!
-    var promiseLoadAnimals: Promise<[AnimalModel]>!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadingEnabled = true
-        tableView.rowHeight = 80
-        tableView.allowsSelection = true
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        if user.isMe {
-            let header = ViewUseful.instanceFromNib("createAnimal") as! CreateAnimalView
-            header.callback = createAnimal
-            let frame = CGRect(x: header.frame.minX, y: header.frame.minY, width: header.frame.width, height: 80)
-            header.frame = frame
-            tableView.tableHeaderView = header
-            
-            tableView.tableHeaderView!.snp.makeConstraints { make in
-                make.height.equalTo(80)
-                make.width.equalTo(self.tableView.width)
-            }
-        }
-    }
-    
-    override func onPopulateCell(item: AnimalModel, cell: AnimalListTableViewCell) {
-        
-        cell.configure(picUrl: item.profilePicUrl, name: item.name)
-        cell.settings.onTap { _ in
-            self.navigationController?.pushViewController(AnimalConfigurationViewController.newInstance(animal: item), animated: true)
-        }
-        cell.setEditable(item.isMine())
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        App.instance.userData.selectedAnimal = indexPath.row
-        
-        let n: Int = navigationController!.viewControllers.count
-        let animalVC = navigationController!.viewControllers[n - 2] as! AnimalVC
-        animalVC.animal = user.animals![indexPath.row]
-        animalVC.reloadAnimal()
-    }
-    
-    override func fetchItems(from: Int, count: Int) -> Promise<[AnimalModel]> {
-        
-        if theData != nil {
-            return Promise(value: [])
-        }
-        return promiseLoadAnimals ?? Promise { fulfill, reject in }
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        // navigationController?.isNavigationBarHidden = true FIXME
-    }
+   
+   var user: UserModel!
+   var createAnimal: (() -> ())!
+   var promiseLoadAnimals: Promise<[AnimalModel]>!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      loadingEnabled = true
+      tableView.rowHeight = 80
+      tableView.allowsSelection = true
+   }
+   
+   override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      
+      if user.isMe {
+         self.title = "Mes animaux"
+         let header = ViewUseful.instanceFromNib("createAnimal") as! CreateAnimalView
+         header.callback = createAnimal
+         let frame = CGRect(x: header.frame.minX, y: header.frame.minY, width: header.frame.width, height: 80)
+         header.frame = frame
+         tableView.tableHeaderView = header
+         
+         tableView.tableHeaderView!.snp.makeConstraints { make in
+            make.height.equalTo(80)
+            make.width.equalTo(self.tableView.width)
+         }
+      }
+      else {
+         self.title = "Animaux"
+      }
+   }
+   
+   override func onPopulateCell(item: AnimalModel, cell: AnimalListTableViewCell) {
+      
+      cell.configure(picUrl: item.profilePicUrl, name: item.name)
+      cell.settings.onTap { _ in
+         self.navigationController?.pushViewController(AnimalConfigurationViewController.newInstance(animal: item), animated: true)
+      }
+      cell.setEditable(item.isMine())
+   }
+   
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
+      App.instance.userData.selectedAnimal = indexPath.row
+      
+      let n: Int = navigationController!.viewControllers.count
+      let animalVC = navigationController!.viewControllers[n - 2] as! AnimalVC
+      animalVC.animal = user.animals![indexPath.row]
+      animalVC.reloadAnimal()
+   }
+   
+   override func fetchItems(from: Int, count: Int) -> Promise<[AnimalModel]> {
+      
+      if theData != nil {
+         return Promise(value: [])
+      }
+      return promiseLoadAnimals ?? Promise { fulfill, reject in }
+   }
+   
+   override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      
+      // navigationController?.isNavigationBarHidden = true FIXME
+   }
 }
--- AnimalsMeet/NewPostViewController.swift
+++ AnimalsMeet/NewPostViewController.swift
@@ -12,107 +12,116 @@ import ARSLineProgress
 import PromiseKit
 
 class NewPostViewController: UIViewController, FusumaDelegate {
-
-    @IBOutlet weak var profilePic: UIImageView!
-    @IBOutlet weak var text: UITextView!
-    @IBOutlet weak var picIcon: UIImageView!
-    @IBOutlet weak var sendButton: UIBarButtonItem!
-    
-    var newMedia: MediaModel!
-    
-    @IBAction func stop(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func getAnimal() -> AnimalModel {
-        return App.instance.userModel.animals![App.instance.userData.selectedAnimal]
-    }
-    
-    func addPicture() {
-        let fusuma = FusumaViewController()
-        fusuma.delegate = self
-        fusuma.hasVideo = false
-        fusuma.allowMultipleSelection = false
-        self.present(fusuma, animated: true, completion: nil)
-    }
-    
-    func postPicture(description: String?) {
-        
-        newMedia.description = text.text
-        
-        newMedia.callForCreate().then { _ -> Void in
-            alert.showAlertSuccess(title: "Nouvelle image", subTitle: "Votre photo a été postée")
-            self.stop(self)
-            }.always {
-                ARSLineProgress.hide()
-        }.catch { err in
+   
+   @IBOutlet weak var profilePic: UIImageView!
+   @IBOutlet weak var text: UITextView!
+   @IBOutlet weak var picIcon: UIImageView!
+   @IBOutlet weak var sendButton: UIBarButtonItem!
+   
+   @IBOutlet weak var photoWidthConstraint: NSLayoutConstraint!
+   @IBOutlet weak var textImageLeadingAligned: NSLayoutConstraint!
+   @IBOutlet weak var textSuperviewLeadingAligned: NSLayoutConstraint!
+   
+   var newMedia: MediaModel!
+   
+   @IBAction func stop(_ sender: Any) {
+      dismiss(animated: true, completion: nil)
+   }
+   
+   func getAnimal() -> AnimalModel {
+      return App.instance.userModel.animals![App.instance.userData.selectedAnimal]
+   }
+   
+   func addPicture() {
+      let fusuma = FusumaViewController()
+      fusuma.delegate = self
+      fusuma.hasVideo = false
+      fusuma.allowMultipleSelection = false
+      self.present(fusuma, animated: true, completion: nil)
+   }
+   
+   func postPicture(description: String?) {
+      
+      newMedia.description = text.text
+      
+      newMedia.callForCreate().then { _ -> Void in
+         alert.showAlertSuccess(title: "Nouvelle image", subTitle: "Votre photo a été postée")
+         self.stop(self)
+         }.always {
+            ARSLineProgress.hide()
+         }.catch { err in
             self.sendButton.isEnabled = true
             App.showRequestFailure(err)
-        }
-    }
-    
-    func sendPost() {
-        _ = Api.instance.post("/media/createPost", withParams: ["content": text.text, "animal_id": getAnimal().id!])
-            .then { _ -> () in
-                alert.showAlertSuccess(title: "Succès", subTitle: "Votre post a été envoyé !")
-                self.stop(self)
-            }
-            .always {
-                ARSLineProgress.hide()
-            }
-            .catch { _ in
-                alert.showAlertError(title: "Erreur", subTitle: "Impossible d'envoyer le post")
-                self.sendButton.isEnabled = true
-        }
-    }
-    
-    @IBAction func send(_ sender: Any) {
-        
-        sendButton.isEnabled = false
-        ARSLineProgress.show()
-        
-        if newMedia != nil {
-            postPicture(description: text.text)
-        } else if !text.text.isEmpty {
-            sendPost()
-        } else {
-            sendButton.isEnabled = true
+      }
+   }
+   
+   func sendPost() {
+      _ = Api.instance.post("/media/createPost", withParams: ["content": text.text, "animal_id": getAnimal().id!])
+         .then { _ -> () in
+            alert.showAlertSuccess(title: "Succès", subTitle: "Votre post a été envoyé !")
+            self.stop(self)
+         }
+         .always {
             ARSLineProgress.hide()
-        }
-    }
-    
-    /* Fusuma */
-    public func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
-        
-        let imageData:Data = UIImageJPEGRepresentation(image, 0.3)!
-        newMedia = MediaModel()
-        
-        let user = App.instance.userModel!
-        newMedia.author = user
-        newMedia.animal = getAnimal()
-        newMedia.rawData = imageData.base64EncodedString(options: .lineLength64Characters) // TODO: specify animal
-        picIcon.image = image
-        picIcon.layer.cornerRadius = 4
-    }
-    
-    func fusumaDismissedWithImage(_ image: UIImage) {}
-    func fusumaVideoCompleted(withFileURL fileURL: URL) {}
-    func fusumaCameraRollUnauthorized() {}
-    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {}
-    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {}
-    @objc func fusumaClosed() {}
-    @objc func fusumaWillClosed() {}
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        profilePic.kf.setImage(with: App.instance.userModel.image)
-        profilePic.layer.cornerRadius = 8
-        picIcon.onTap { _ in
-            let vc = FusumaViewController()
-            vc.delegate = self
-            vc.hasVideo = false
-            self.present(vc, animated: true)
-        }
-    }
+         }
+         .catch { _ in
+            alert.showAlertError(title: "Erreur", subTitle: "Impossible d'envoyer le post")
+            self.sendButton.isEnabled = true
+      }
+   }
+   
+   @IBAction func send(_ sender: Any) {
+      
+      sendButton.isEnabled = false
+      ARSLineProgress.show()
+      
+      if newMedia != nil {
+         postPicture(description: text.text)
+      } else if !text.text.isEmpty {
+         sendPost()
+      } else {
+         sendButton.isEnabled = true
+         ARSLineProgress.hide()
+      }
+   }
+   
+   /* Fusuma */
+   public func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+      
+      let imageData:Data = UIImageJPEGRepresentation(image, 0.3)!
+      newMedia = MediaModel()
+      
+      let user = App.instance.userModel!
+      newMedia.author = user
+      newMedia.animal = getAnimal()
+      newMedia.rawData = imageData.base64EncodedString(options: .lineLength64Characters) // TODO: specify animal
+      picIcon.image = image
+      picIcon.layer.cornerRadius = 4
+      
+      self.photoWidthConstraint.isActive = false
+      self.text.placeholder = "Écrire une légende…"
+      self.textSuperviewLeadingAligned.isActive = false
+      self.textImageLeadingAligned.isActive = true
+   }
+   
+   func fusumaDismissedWithImage(_ image: UIImage) {}
+   func fusumaVideoCompleted(withFileURL fileURL: URL) {}
+   func fusumaCameraRollUnauthorized() {}
+   func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {}
+   func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {}
+   @objc func fusumaClosed() {}
+   @objc func fusumaWillClosed() {}
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      profilePic.kf.setImage(with: App.instance.userModel.image)
+      profilePic.layer.cornerRadius = 8
+      picIcon.onTap { _ in
+         let vc = FusumaViewController()
+         vc.delegate = self
+         vc.hasVideo = false
+         self.present(vc, animated: true)
+      }
+   }
 }
