e901f5d9378da70b6f2870a8f0e103f58c761052
kjoe <kjoe@imac-de-kjoe.local>
2017-11-01 10:06:59 -0400

    updating bundle identifier to com.clechevalllier  to boil with buddy build

--- AnimalsMeet/AppDelegate.swift
+++ AnimalsMeet/AppDelegate.swift
@@ -32,9 +32,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         #if OFFLINE
             Api.instance.serverUrl = "http://localhost:3000/v1"
         #elseif DEBUG
-            Api.instance.serverUrl = "http://52.169.82.167/v1"
+            Api.instance.serverUrl = "http://52.169.82.167"
         #else
-            Api.instance.serverUrl = "http://api.animals-meet.com/v1"
+            Api.instance.serverUrl = "http://52.169.82.167"
         #endif
         App.instance.loadUserData()
         
